710e7098fa991d9ca96df7abc6ca97d7c5ab8876
Dan Vernon <danvernon@gmail.com>
2019-02-20 13:55:14 +0000

    restaurant/review model validations populated

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
   has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :phone_number, presence: true#, numericality: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+
+
 end
